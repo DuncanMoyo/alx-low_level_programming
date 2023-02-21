5bd66ff2df8a857bd7c29f8a75b0e2a6fb298418
DuncanMoyo <duncanfmoyo@gmail.com>
2023-02-21 12:33:00 -0800

    Ommited parameter name

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,14 +3,12 @@
 /**
  * _abs - Entry point
  * Description: a function that computes the absolute value of an integer.
- * @int: number to be compared
+ * @c: number to be compared
  * Return: 0 (success)
  */
 
-int _abs(int)
+int _abs(int c)
 {
-	int c;
-
 	if (c >= 0)
 	{
 		_putchar(c);
