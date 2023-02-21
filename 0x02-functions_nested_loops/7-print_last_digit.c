af1d263742afa9f72fbd333676bf612a13f6b7eb
DuncanMoyo <duncanfmoyo@gmail.com>
2023-02-21 13:16:04 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,15 @@
 /**
  * print_last_digit - Entry point
  * Description: a function that prints the last digit of a number
- * @c: value of parameter
+ * @n: value of parameter
  * Return: last digit of the parameter
  */
 
-int print_last_digit(int c)
+int print_last_digit(int n)
 {
-	int last_digit = c % 10;
+	int last_digit = n % 10;
 
-	if (c < 0)
+	if (n < 0)
 	{
 		last *= -1
 	}
