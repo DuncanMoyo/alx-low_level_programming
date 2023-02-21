6b242606454c5339d7ac00f28acdb0315af32447
DuncanMoyo <duncanfmoyo@gmail.com>
2023-02-21 13:08:28 -0800

    trying to fix the parameter name ommitted error

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,15 @@
 /**
  * print_last_digit - Entry point
  * Description: a function that prints the last digit of a number
- * @x: value of parameter
+ * @c: value of parameter
  * Return: last digit of the parameter
  */
 
-int print_last_digit(int x)
+int print_last_digit(int c)
 {
-	int last_digit = x % 10;
+	int last_digit = c % 10;
 
-	if (x < 0)
+	if (c < 0)
 	{
 		_putchar(-last_digit);
 	}
