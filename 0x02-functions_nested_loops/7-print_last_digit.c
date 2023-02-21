83813543a2939cdb9446c84d8bea1da8d5eb50dc
DuncanMoyo <duncanfmoyo@gmail.com>
2023-02-21 14:03:48 -0800

    Im federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,19 +2,20 @@
 
 /**
  * print_last_digit - Entry point
- * @n: The number in question.
- * Description: prints the last digit of a number
- * Return: Value of the last digit.
+ * Description: a function that prints the last digit of a number
+ * @x: value of parameter
+ * Return: last digit of the parameter
  */
 
-int print_last_digit(int n)
+int print_last_digit(int x)
 {
-	int last_digit = n % 10;
+	int last_digit = x % 10;
 
-	if (last_digit < 0)
-		last_digit *= -1;
-
-	_putchar(last_digit + '0');
-
-	return (last_digit);
+	if (x >= 0)
+	{
+		return (last_digit);
+	}
+	{
+		return (-last_digit);
+	}
 }
