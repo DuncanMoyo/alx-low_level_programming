2455611235038a0e9183823cbe7b762b9689d395
DuncanMoyo <duncanfmoyo@gmail.com>
2023-02-21 16:19:06 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ * Description: prints all natural numbers from n to 98
+ * @n: integer that is to be assessed
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n; n <= 98; n++)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			} else
+			{
+				printf("%d", n);
+			}
+		}
+	} else
+	{
+		for (n; n >= 98; n--)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			} else
+			{
+				printf("%d", n);
+			}
+		}
+	}
+}
