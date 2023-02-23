1f3f0b7c30f6e6dfe636e73021d65d2c0a143de3
DuncanMoyo <duncanfmoyo@gmail.com>
2023-02-23 12:24:27 -0800

    Task 10 - forgot to replace size as a variable

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,14 +10,14 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	if (n <= 0)
+	if (size <= 0)
 	{
 		printf("\n");
 	} else
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = i; j < n; j++)
+			for (j = i; j < size; j++)
 			{
 				printf(" ");
 			}
