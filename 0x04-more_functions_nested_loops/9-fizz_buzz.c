8661d33c402546967a54531565f2fdf58c3c5df0
DuncanMoyo <duncanfmoyo@gmail.com>
2023-02-23 07:07:29 -0800

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz");
+		} else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		} else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		} else if (i < 100)
+		{
+			printf(" ");
+		}
+		{
+			printf("%d", i);
+		}
+	}
+	return (0);
+}
