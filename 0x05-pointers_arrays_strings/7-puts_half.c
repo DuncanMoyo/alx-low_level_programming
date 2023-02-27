14e86e17a7c4d69c83a27c5059d65a6b9cc3b102
DuncanMoyo <duncanfmoyo@gmail.com>
2023-02-27 23:11:30 +0000

    Task 7 - removed curly braces

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,19 +13,18 @@ void puts_half(char *str)
 	string = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
+
 		string++;
+
+	j = (string / 2)
+
+	if (string % 2 == 1)
+	{
+		j = ((string + 1) / 2);
 	}
-		j = (string / 2)
-
-			if (string % 2 == 1)
-		{
-			j = ((string + 1) / 2);
-		}
-		for (i = j; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-
-		_putchar('\n');
+	for (i = j; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
