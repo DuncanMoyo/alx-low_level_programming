ff3abcfe9e78aff548e442894c297e1142782da8
DuncanMoyo <duncanfmoyo@gmail.com>
2023-02-27 23:07:20 +0000

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ * Description: a function that prints half of a string
+ * @str: string to be assessed
+ */
+
+void puts_half(char *str)
+{
+	int i, j, string;
+
+	string = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		string++;
+	}
+		j = (string / 2)
+
+			if (string % 2 == 1)
+		{
+			j = ((string + 1) / 2);
+		}
+		for (i = j; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+
+		_putchar('\n');
+}
