708c0f7c28d6fa748f6936ae6e1f5a221d4bdb6f
DuncanMoyo <duncanfmoyo@gmail.com>
2023-03-02 10:07:26 +0000

    Task 2 - switched it up a bit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,17 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
 	}
+	return (dest);
 }
