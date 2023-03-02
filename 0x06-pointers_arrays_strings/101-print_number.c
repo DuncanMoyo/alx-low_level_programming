d8dcea941cbb6e15c80f01aad1843f9c0525998f
DuncanMoyo <duncanfmoyo@gmail.com>
2023-03-02 14:16:15 +0000

    It is the addition of strangeness to beauty that constitutes the romantic character in art

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * Description: Function that prints out an integer
+ * @n: Number to print
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n > 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	i = n;
+
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
