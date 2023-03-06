dbbdd7bc842ff0e9fcfde798152f5919259781ae
DuncanMoyo <duncanfmoyo@gmail.com>
2023-03-06 18:45:54 +0000

    Task 3 - Added IF statement

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,14 +17,16 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (i = 0; accept[i]; i++)
 		{
-			j++;
-			break;
-		}
-		else if ((accept[i + 1]) == '\0')
-		{
-			return (j);
-		}
+			if (accept == *s)
+			{
+				j++;
+				break;
+			} else if ((accept[i + 1]) == '\0')
+			{
+				return (j);
+			}
 		s++;
+		}
 	}
 	return (j);
 }
