d8e0a2e7f5bb1986b12c5958d7eb4eeb87339876
DuncanMoyo <duncanfmoyo@gmail.com>
2023-03-20 21:42:50 +0000

    Added an IF statement in case d is NULL

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdio.h>
 /**
  * init_dog - initialises the struct parameters
  * @d: struct
@@ -10,6 +10,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		printf("Error: Null pointer passed to init_dog\n");
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
