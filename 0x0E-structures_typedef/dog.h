3668f6d041bf8daaf325b97b5d590f1eb63baaba
DuncanMoyo <duncanfmoyo@gmail.com>
2023-03-20 21:12:18 +0000

    Task 0 - Added comments and followed Betty styling convention

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef MAIN
 #define MAIN
 
-struct dog {
+/**
+ * struct dog - a dog's basic info
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
