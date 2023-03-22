d70ed4d215f4cd0c8291a3f96efcf26a6f697256
DuncanMoyo <duncanfmoyo@gmail.com>
2023-03-22 15:48:13 +0000

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function chosen by the user
+ * @s: operator passed
+ * Return: function that matches the operator and NULL if the operator passed
+ * doesn't match the function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (ops[i].op != NULL)
+	{
+		if (*ops[i].op == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: Added result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: Subtracted result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_mul - multiplies two parameters
+ * @a: first parameter
+ * @b: second parameter
+ * Return: Multiplied result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides using the two parameters
+ * @a: first parameters
+ * @b: second parameter
+ * Return: Result of division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - gives the remainder of a divided number
+ * @a: first parameter
+ * @b: second parameter
+ * Return: The remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
