a3cfb2ba165ba76a195ead63254484161002d557
DuncanMoyo <duncanfmoyo@gmail.com>
2023-03-30 22:11:19 +0000

    task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s\n", 0
+
+	SECTION .text
+	extern printf
+	global main
+
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
+
