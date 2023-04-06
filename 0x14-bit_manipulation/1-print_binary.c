d0b08bc2196d2360adf7667afdeef69d44047765
DuncanMoyo <duncanfmoyo@gmail.com>
2023-04-06 10:18:50 +0000

    task 1: added comments

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,9 @@ void print_binary(unsigned long int n)
 {
 	if (n > 1)
 	{
+		/* Divides n by 2. Shifts bits in n by 1 pos to the right */
 		print_binary(n >> 1);
 	}
+	/* prints the least bit of n. '0' converts to to a character */
 	_putchar((n & 1) + '0');
 }
