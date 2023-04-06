548f8179da3a74e7600709aa4d11f3372ba31f20
DuncanMoyo <duncanfmoyo@gmail.com>
2023-04-06 21:19:12 +0000

    task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness.
+ * Return: int
+ */
+
+int get_endianness(void)
+{
+	/* i is represented in big and little endian in binary  */
+	unsigned int i = 1;
+	/* when c is dereferenced, we have the first byte of c only */
+	char *c = (char *) &i;
+	/* dereferences c, gets the 1st byte of i, casts it to an int  */
+	/* and returns it */
+	return ((int)*c);
+}
