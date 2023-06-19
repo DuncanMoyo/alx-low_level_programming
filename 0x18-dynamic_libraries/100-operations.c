4f12032b6df328952b7e57fedc31e1cbd33b9f68
DuncanMoyo <duncanfmoyo@gmail.com>
2023-06-19 13:48:25 +0000

    Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+
+/**
+* add - adds two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the sum
+*/
+
+extern int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtracts two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the subtract
+*/
+
+extern int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiplies two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the multiple
+*/
+
+extern int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divideds two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the dividend integer
+*/
+
+extern int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds the modulus of two integers
+ * @a: first integer to add
+ * @b: second integer to add
+ * Return: the modulus
+*/
+
+extern int mod(int a, int b)
+{
+	return (a % b);
+}
