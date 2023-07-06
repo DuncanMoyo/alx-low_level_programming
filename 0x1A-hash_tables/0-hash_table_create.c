c1564592ffc5bc8a7823bde147b77358a633946e
DuncanMoyo <duncanfmoyo@gmail.com>
2023-07-06 12:49:23 +0000

    Task 0 & 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_t;
+
+	if (size == 0)
+		return (NULL);
+
+	hash_t = malloc(sizeof(hash_table_t));
+
+	if (hash_t == NULL)
+		return (NULL);
+
+	hash_t->array = calloc(size, sizeof(hash_node_t *));
+	if (hash_t->array == NULL)
+	{
+		free(hash_t);
+		return (NULL);
+	}
+
+	hash_t->size = size;
+
+	return (hash_t);
+}
